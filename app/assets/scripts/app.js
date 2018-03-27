b9b864667d19e3b73b2578e11caa791dec587d26
Lauren <laurenekcoker@gmail.com>
2018-03-27 16:00:59 -0500

    Started nav bar, finished large hero, started services section

---
+++ app/assets/scripts/app.js
@@ -0,0 +1,10 @@
+import jarallax from "jarallax";
+
+jarallax(document.querySelectorAll('.jarallax'), {
+    disableParallax: function () {
+        return /iPad|iPhone|iPod|Android/.test(navigator.userAgent);
+    },
+    disableVideo: function () {
+        return /iPad|iPhone|iPod|Android/.test(navigator.userAgent);
+    }
+});
