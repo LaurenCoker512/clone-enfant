c7c4e9851d1fa597967f8c926972304e6acf37f1
Lauren <laurenekcoker@gmail.com>
2018-03-30 15:27:23 -0500

    Added direction-aware hover effect to gallery

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,4 +1,47 @@
 import jarallax from "jarallax";
+import $ from "jquery";
+
+//Direction-aware hover
+
+$('.gallery__slider__img').on('mouseenter mouseleave', hoverDirection);
+
+function hoverDirection(event) {
+  var $overlay = $(this).find('div'),
+      side = getMouseDirection(event),
+      animateTo,
+      positionIn = {
+        top: '0%',
+        left: '0%'
+      },
+      positionOut = (function() {
+        switch(side) {
+          case 0:  return { top: '-100%', left:    '0%' }; break;
+          case 1:  return { top:    '0%', left:  '100%' }; break;
+          case 2:  return { top:  '100%', left:    '0%' }; break;
+          default: return { top:    '0%', left: '-100%' }; break;
+        }
+      })();
+  if ( event.type === 'mouseenter' ) {
+    animateTo = positionIn;
+    $overlay.css(positionOut);
+  } else {
+    animateTo = positionOut;
+  }
+  $overlay.stop(true).animate(animateTo, 200, 'linear');
+}
+
+function getMouseDirection(event) {
+  var $item = $(event.currentTarget),
+      offset = $item.offset(),
+      w = $item.outerWidth(),
+      h = $item.outerHeight(),
+      x = (event.pageX - offset.left - w / 2) * ((w > h) ? h / w: 1),
+      y = (event.pageY - offset.top - h / 2) * ((h > w) ? w / h: 1),
+      direction = Math.round((Math.atan2(y, x) * (180 / Math.PI) + 180) / 90  + 3) % 4;
+  return direction;
+}
+
+//Parallax scrolling
 
 jarallax(document.querySelectorAll('.jarallax'), {
     disableParallax: function () {
@@ -8,3 +51,4 @@ jarallax(document.querySelectorAll('.jarallax'), {
         return /iPad|iPhone|iPod|Android/.test(navigator.userAgent);
     }
 });
+
